0f2692b70d8e4cdb229ef2571f86e4f6380185fc
Vinecreeper888 <siddhanth8@gmail.com>
2021-01-05 12:19:19 +0530

    Updates

---
+++ precache-manifest.510bfc45bc7c96d08e8f0e3f14463a5b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb875938970aef6ca0ee",
+    "url": "/catch-of-the-day/static/css/main.f4eba2c8.chunk.css"
+  },
+  {
+    "revision": "cb875938970aef6ca0ee",
+    "url": "/catch-of-the-day/static/js/main.3ade0e01.chunk.js"
+  },
+  {
+    "revision": "08bfa76b6b44b31ce8c9",
+    "url": "/catch-of-the-day/static/js/runtime~main.f69863a0.js"
+  },
+  {
+    "revision": "a2c3d63eac26b57c565c",
+    "url": "/catch-of-the-day/static/js/2.25ba0bcd.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/catch-of-the-day/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "2bd23bb4a00094601e7a6a23b58d622e",
+    "url": "/catch-of-the-day/index.html"
+  }
+];
